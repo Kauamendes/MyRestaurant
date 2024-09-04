7b4c703693fc1091386a82b575f5c21991cb717d
Kauã Mendes <kauamendes80@hotmail.com>
2024-09-04 19:54:47 -0300

    commit inicial

---
+++ MyRestaurant/src/test/java/br/com/myrestaurant/MyRestaurant/MyRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.myrestaurant.MyRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
